6a7671d9a8930f2bc9f53aceec017ac523b6f26f
buclestudio <126496763+buclestudio@users.noreply.github.com>
2023-03-01 01:29:09 -0300

    /js.
    En proceos

---
+++ script.js
@@ -0,0 +1,11 @@
+const $portfolio =document.querySelector ("sec-portfolio-js"
+)
+$portfolio .addEventListener ("click",(e)=> {
+    //console.log(e.target.classlist);
+    //(e.target.classlist. classlist);
+    if  (e.target.classlist.contains ('img-btn-modal-js')) {
+        //SRC
+            
+        //Add Modal
+    }
+});
